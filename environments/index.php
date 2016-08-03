cc4c10a25ac015e2a4578ce97095304de6811697
Mateusz Marciniszyn <marciniszyn@pwapm.com>
2016-08-03 19:19:22 +0200

    try heroku

--- environments/index.php
+++ environments/index.php
@@ -62,4 +62,20 @@ return [
             'frontend/config/main-local.php',
         ],
     ],
+    'Heroku' => [
+        'path' => 'heroku',
+        'setWritable' => [
+            'backend/runtime',
+            'backend/web/assets',
+            'frontend/runtime',
+            'frontend/web/assets',
+        ],
+        'setExecutable' => [
+            'yii',
+        ],
+        'setCookieValidationKey' => [
+            'backend/config/main-local.php',
+            'frontend/config/main-local.php',
+        ],
+    ],
 ];

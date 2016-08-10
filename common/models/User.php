d159232afb5d883b7fc4b9124d80c97c73035f21
Tycjan Sobel <Tycjan Sobel>
2016-08-10 21:06:05 +0200

    fix username

--- common/models/User.php
+++ common/models/User.php
@@ -81,7 +81,7 @@ class User extends ActiveRecord implements IdentityInterface
      * @param string $username
      * @return static|null
      */
-    public static function findByUsrname($username)
+    public static function findByUsername($username)
     {
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }

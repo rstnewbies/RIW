29e3ea6bb285175fd8a91e0aaba5e59672352236
Tycjan Sobel <mrtycjan@protonmail.ch>
2016-08-24 16:53:59 +0200

    remove merge conf

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -18,25 +18,16 @@ $this->title = 'My Yii Application';
                 <?php echo Html::a("Group", ["group/index"],['class'=>'btn btn-lg btn-warning dashboard-btn']); ?>  
             </div>
             <div class="col-lg-4 dashboard-div-btn">
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2b246d11aba863719d2a42255ede141941005b90
+
                 <?php echo Html::a("Task", ["task/index"],['class'=>'btn btn-lg btn-warning dashboard-btn']); ?>  
             </div>
         </div>
         <div class="row">
             <div class="col-lg-4 dashboard-div-btn">
                 <?php echo Html::a("Code", ["code/index"],['class'=>'btn btn-lg btn-warning dashboard-btn']); ?>  
-<<<<<<< HEAD
-=======
-                <?php echo Html::a("Time", ["time/update",'id'=>'1'],['class'=>'btn btn-lg btn-warning dashboard-btn']); ?>  
->>>>>>> feature/zegatek
-=======
             </div>
             <div class="col-lg-4 dashboard-div-btn">
                  <?php echo Html::a("Time", ["time/update",'id'=>'1'],['class'=>'btn btn-lg btn-warning dashboard-btn']); ?>  
->>>>>>> 2b246d11aba863719d2a42255ede141941005b90
             </div>
         </div>
 

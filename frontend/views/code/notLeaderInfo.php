df027f5b1c621ca8f037f5c5c74da1818ecd1258
Mateusz Marciniszyn <mmmarciniszyn@gmail.com>
2016-09-28 08:38:09 +0200

    fix bug no-leader reader

--- frontend/views/code/notLeaderInfo.php
+++ frontend/views/code/notLeaderInfo.php
@@ -1,2 +1,18 @@
+<?php
+use yii\helpers\Html;
+use common\models\User;
+?>
+<div class="site-about">
+    <div class="col-lg-11 text-center resetPassword">
+        <h1> Tylko lider drużyny może wpisywać kody.</h1> 
+        <h2> A ty nim nie jesteś.</h2>
+        <h1> Twoim liderem jest
+        <?php 
+        $leaderGroup = User::find()->where(['group_id' => Yii::$app->user->identity->group_id])->orderBy("leader_points DESC")->one();
+        echo $leaderGroup->name . " " . $leaderGroup->last_name;
+        ?>
+        </h1>
+    </div> 
+</div>
 
     

6cad31feda3c9a2d1182e94d4ee66fc5ec47713d
Mateusz Marciniszyn <mmmarciniszyn@gmail.com>
2016-10-05 11:16:18 +0200

    try email

--- frontend/models/PasswordResetRequestForm.php
+++ frontend/models/PasswordResetRequestForm.php
@@ -60,9 +60,9 @@ class PasswordResetRequestForm extends Model
                 ['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'],
                 ['user' => $user]
             )
-            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
+            ->setFrom(Yii::$app->params['supportEmail'])
             ->setTo($this->email)
-            ->setSubject('Password reset for ' . Yii::$app->name)
+            ->setSubject('Zmien haslo do KompasRST')
             ->send();
     }
 }
